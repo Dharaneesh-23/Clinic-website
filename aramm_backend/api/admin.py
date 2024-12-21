119ddff7f63fa44801e6a67916e094a6d2441b51
Dharaneesh <108232673+Dharaneesh-23@users.noreply.github.com>
2024-12-21 23:57:40 +0530

    Delete aramm_backend directory

--- aramm_backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PatientDetails,Administration,PackageDetails,Reviews
-
-admin.site.register(PatientDetails)
-admin.site.register(Administration)
-admin.site.register(PackageDetails)
-admin.site.register(Reviews)
-# Register your models here.
